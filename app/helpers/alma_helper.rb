a5eef3477973600a19ed8dff3f35afcefacea43a
Josh Weisman <josh.m.weisman@gmail.com>
2015-10-13 21:20:08 +0300

    Minor refactor
    Move open URL to a link

--- app/helpers/alma_helper.rb
+++ app/helpers/alma_helper.rb
@@ -1,7 +1,7 @@
 module AlmaHelper
-	def openurl
-		"https://#{ENV['alma']}.alma.exlibrisgroup.com/view/uresolver/#{ENV['institution']}/openurl?rfr_id=info:sid/primo.exlibrisgroup.com&u.ignore_date_coverage=true&" +
-		 	(current_user && current_user.provider=='saml' ? "sso=true&token=#{session.id}&" : "") 
+	def openurl(mms_id, service='viewit')
+		"https://#{ENV['alma']}.alma.exlibrisgroup.com/view/uresolver/#{ENV['institution']}/openurl?rfr_id=info:sid/primo.exlibrisgroup.com&u.ignore_date_coverage=true&svc_dat=#{service}&rft.mms_id=#{mms_id}" +
+		 	(current_user && current_user.provider=='saml' ? "&sso=true&token=#{session.id}" : "") 
 	end
 
 	def thumburl(id)
