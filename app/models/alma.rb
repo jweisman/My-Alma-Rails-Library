89e31d82a398698adc3e0f9058624419eca0fbbf
Josh Weisman <josh.m.weisman@gmail.com>
2015-11-05 12:43:15 +0200

    Implement multi-BIB RTA check
    https://developers.exlibrisgroup.com/alma/apis/bibs/GET/gwPcGly021q2Z+qBbnVJzw==/af2fb69d-64f4-42bc-bb05-d8a0ae56936e

--- app/models/alma.rb
+++ app/models/alma.rb
@@ -40,25 +40,30 @@ module Alma
 
 	# Alma helper methods
 
-	def alma_get_bib_availability(mms_id)
+	def alma_get_bibs_availability(mms_id)
 		response = 
 		RestClient.get ENV['almaurl'] + 
-			"/bibs/#{mms_id}?expand=p_avail,e_avail,d_avail",
+			"/bibs?mms_id=#{mms_id}&expand=p_avail,e_avail,d_avail",
 			authorization: 'apikey ' + ENV['apikey']
 
-		bib = Nokogiri::XML(response)
-		print = bib.at_xpath('/bib/record/datafield[@tag="AVA"]/subfield[@code="e"]')
-		digital = bib.at_xpath('/bib/record/datafield[@tag="AVD"]')
-		electronic = bib.at_xpath('/bib/record/datafield[@tag="AVE"]')
+		xml = Nokogiri::XML(response)
+		bibs = {}
+		bib_nodes = xml.xpath('/bibs/bib')
+		bib_nodes.each do |bib_node|
+			print = bib_node.at_xpath('record/datafield[@tag="AVA"]/subfield[@code="e"]')
+			digital = bib_node.at_xpath('record/datafield[@tag="AVD"]')
+			electronic = bib_node.at_xpath('record/datafield[@tag="AVE"]')
 
-	    { :physical => 
-	    	{ :exists => print ? true : false,
-	    	  :available => print && print.text == 'available' ? true : false
-	    	},
-	      :online   => 
-	      	{ :exists => digital || electronic ? true : false
-	      	}
-	    }
+			bibs[bib_node.at_xpath('mms_id').text] = 
+				{ :physical => 
+					{ 	:exists => print ? true : false,
+						:available => print && print.text == 'available' ? true : false
+					},
+				   :online   => 
+					{ :exists => digital || electronic ? true : false }
+				}
+		end
+
+		return bibs
 	end
-  	
 end
